27120193194e1771a3f64bafabee61eec87df121
Luis Daniel Olvera Díaz <luisdanielolvera@MacBook-Pro-de-Luis.local>
2026-04-13 15:11:19 -0600

    Configuration server Vite Api and add images from Api

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
+
+// Aquí vamos a crear nuestro primer componente de React
+
+const App = () => {
+  return (
+    <div>
+      <h1>Padre Gino's - Order - Now</h1>
+      <Pizza
+        name="Pepperoni"
+        description="pep, cheese, n styff"
+        image={"public/pizzas/pepperoni.webp"}
+      />
+      <Pizza
+        name="Hawaiian"
+        description="ham, pineapple, n stuff"
+        image={"public/pizzas/hawaiian.webp"}
+      />
+      <Pizza
+        name="Americano"
+        description="french fries, hot dogs, n stuff"
+        image={"public/pizzas/big_meat.webp"}
+      />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
---
+++ src/Pizza.jsx
@@ -0,0 +1,22 @@
+// Vamos a hacer nuestro componente de pizza
+
+/* const pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+}; */
+
+// Vamos a reemplazarlo con JSX.
+
+const Pizza = (props) => {
+  return (
+    <div className="pizza">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
+};
+
+export default Pizza;
